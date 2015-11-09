3b6f74cca55fa9a51c5cb37a2925cd0fb523f100
Gustave Abel Michel III <gus3@michelfamily>
2015-11-09 21:52:37 +0000

    Switch to using new API calls for Marker Coords

--- Nodejs/public/javascripts/MapInterface.js
+++ Nodejs/public/javascripts/MapInterface.js
@@ -27,12 +27,15 @@ google.maps.event.addDomListener(window, 'load', function() {
 
 	//Drag Adjust Events
 	google.maps.event.addListener(user_marker, 'mouseup', function() {
-		$.getJSON('/api/user/'+key+'/'+user_marker.position.A+','+user_marker.position.F, function(json_data){
+		
+	$.getJSON('/api/user/'+key+'/'+user_marker.getPosition().lat()+','+user_marker.getPosition().lng(), 
+	function(json_data){
 			dish_marker.setPosition(new google.maps.LatLng(json_data.lat,json_data.lon));
 		});
 	});
 	google.maps.event.addListener(dish_marker, 'mouseup', function() {
-		$.getJSON('/api/dish/'+key+'/'+dish_marker.position.A+'/'+dish_marker.position.F, function(json_data){
+	$.getJSON('/api/dish/'+key+'/'+dish_marker.getPosition().lat()+'/'+dish_marker.getPosition().lng(), 
+	function(json_data){
 			user_marker.setPosition(new google.maps.LatLng(json_data.lat,json_data.lon));
 		});
 	});
